1ebe6561381da36c18c267dcaab0921430e3c4f6
Shakshor <shakshor584@gmail.com>
2022-06-11 18:14:12 +0600

    manage doctor page created

---
+++ src/Pages/Dashboard/DoctorRow.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const DoctorRow = ({ doctor, index }) => {
+    const { name, specialty, img } = doctor;
+
+
+
+    return (
+        <tr>
+            <th>{index + 1}</th>
+            <td>
+                <div class="avatar">
+                    <div class="w-20 rounded">
+                        <img src={img} alt={name} />
+                    </div>
+                </div>
+            </td>
+            <td>{name}</td>
+            <td>{specialty}</td>
+            <td><button className='btn btn-xs btn-error'>delete</button></td>
+        </tr>
+    );
+};
+
+export default DoctorRow;
